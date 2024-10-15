9cd83f69dc5fa05e92a8a346c8479ee225a63d75
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2024-10-14 23:55:33 -0300

    encapsulate tokens

--- src/lexer.rs
+++ src/lexer.rs
@@ -29,47 +29,49 @@ static MATCH_BINARY_OPERATOR: LazyLock<Regex> = LazyLock::new(|| {
 
 static MATCH_PAREN: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^[()]").unwrap());
 
-fn token(token_type: TokenType, val: String) -> Token {
-    Token { token_type, val }
+impl Token {
+    fn new(token_type: TokenType, val: String) -> Self {
+        Self { token_type, val }
+    }
 }
 
 pub fn tokenize(source_string: &str) -> VecDeque<Token> {
     let mut cursor = 0;
-    let mut token_array = VecDeque::new();
+    let mut tokens = VecDeque::new();
     while cursor < source_string.len() {
         if let Some(val) = MATCH_NUMBER
             .find(&source_string[cursor..])
             .map(|x| x.as_str())
         {
             cursor += val.len();
-            token_array.push_back(token(TokenType::Number, val.to_string()));
+            tokens.push_back(Token::new(TokenType::Number, val.to_string()));
         } else if let Some(val) = MATCH_FIELD_STRING
             .find(&source_string[cursor..])
             .map(|x| x.as_str())
         {
             cursor += val.len();
-            token_array.push_back(token(TokenType::String, val.to_string()));
+            tokens.push_back(Token::new(TokenType::String, val.to_string()));
         } else if let Some(val) = MATCH_STRING
             .find(&source_string[cursor..])
             .map(|x| x.as_str().replace("'", "\""))
         {
             cursor += val.len();
-            token_array.push_back(token(TokenType::String, val));
+            tokens.push_back(Token::new(TokenType::String, val));
         } else if let Some(val) = MATCH_BINARY_OPERATOR
             .find(&source_string[cursor..])
             .map(|x| x.as_str())
         {
             cursor += val.len();
-            token_array.push_back(token(TokenType::Binary, val.to_string()));
+            tokens.push_back(Token::new(TokenType::Binary, val.to_string()));
         } else if let Some(val) = MATCH_PAREN
             .find(&source_string[cursor..])
             .map(|x| x.as_str())
         {
             cursor += val.len();
-            token_array.push_back(token(TokenType::Paren, val.to_string()));
+            tokens.push_back(Token::new(TokenType::Paren, val.to_string()));
         } else {
             cursor += 1;
         }
     }
-    token_array
+    tokens
 }
